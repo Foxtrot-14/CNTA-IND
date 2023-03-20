1e30462a68418d19524258f8075195b822d40ee8
Noaman <noamanahmedai@gmail.com>
2023-03-20 09:41:41 +0530

    child details endpoint created

--- child/urls.py
+++ child/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from child.views import *
 urlpatterns = [
     path('child/', AddChildView.as_view(),name='childadd'),
-    #path('child-<pk>', ViewChild.as_view(),name='child'),
+    path('child/<int:pk>/', ChildDetailAPIView.as_view(), name='getchild'),
 ]
